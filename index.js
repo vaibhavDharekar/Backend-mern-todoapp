6c0c23d30bd9764e427fbd6d8aa1d1ee5cc5d334
vaibhavDharekar <vdharekar411@gmail.com>
2023-11-11 14:24:38 +0530

    Install dependencies and setup express server

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const app = express();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(8080,()=>{
+    console.log('listening on port 8080');
+})
