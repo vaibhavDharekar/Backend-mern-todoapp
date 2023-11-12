e25601ab2cce9b572e8fdd78e4e7c46b0bf4bfaa
vaibhavDharekar <vdharekar411@gmail.com>
2023-11-12 23:00:20 +0530

    Signin with JWT complete

--- index.js
+++ index.js
@@ -1,14 +1,24 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const cors = require("cors");
+require('dotenv').config();
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
+app.use(cors({
+    origin: 'http://localhost:3000',  // Replace with the URL of your React app
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+    optionsSuccessStatus: 204,
+  }))
 
-let User = require('./models/user');
 
-mongoose.connect('mongodb://127.0.0.1:27017/fullstackTodoApp')
+let User = require('./models/User');
+
+mongoose.connect('mongodb://127.0.0.1:27017/fullstackTodoApp_')
 .then(()=>{
     console.log('Database connection successful!')
 })
@@ -17,7 +27,7 @@ mongoose.connect('mongodb://127.0.0.1:27017/fullstackTodoApp')
 })
 
 app.post('/signup',async(req,res)=>{
-    let {email,password,firstName,lastName} = req.body;
+    let {email,password,firstName,lastName} = req.body;console.log(email,password,firstName,lastName)
     let newUser = await User.findOne({email});
     if(newUser){
         return res.status(404).json({error:"User already exist!"})
@@ -32,22 +42,26 @@ app.post('/signup',async(req,res)=>{
     }
 })
 
+app.post('/signin',async(req,res)=>{
+    let {email,password} = req.body;
+    let userExist = await User.findOne({email});
+    if(userExist){
+        let correctPassword = await bcrypt.compare(password,userExist.password);
+        if(correctPassword){
+            let token = jwt.sign({_id:userExist._id},process.env.secret);
+            return res.status(201).json({token,email});
+        }
+        else{
+            return res.status(401).json({error:"Incorrect email or password!"});
+        }
+    }
+    else{
+        return res.status(401).json({error:"You need to signup first!"});
+    }
+})
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-app.listen(8080,()=>{
+app.listen(process.env.port,()=>{
     console.log('listening on port 8080');
 })
